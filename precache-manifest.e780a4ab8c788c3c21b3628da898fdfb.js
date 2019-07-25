5bcf620c9d35b116018cc73ab86ddfeee21504a2
Joanne Badua <joannebadua@gmail.com>
2019-07-25 15:17:35 -0700

    Updates

---
+++ precache-manifest.e780a4ab8c788c3c21b3628da898fdfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b329c4f2c9194e462a3963a2852b7a33",
+    "url": "/clicky-yoga-game/index.html"
+  },
+  {
+    "revision": "1aed7d0d4e0c598c4597",
+    "url": "/clicky-yoga-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clicky-yoga-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1aed7d0d4e0c598c4597",
+    "url": "/clicky-yoga-game/static/js/main.bebc64d6.chunk.js"
+  },
+  {
+    "revision": "59209fe97510641d9b98",
+    "url": "/clicky-yoga-game/static/js/runtime~main.8c741244.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-yoga-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
