3a38784de7fb87b220718ef1921ac9f937806f3c
Joanne Badua <joannebadua@gmail.com>
2019-07-25 19:08:55 -0700

    Added pics and on-click function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import pic1 from './pic-1.png';
+import pic2 from './pic-2.png';
+import pic3 from './pic-3.png';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    picArray: [
+      {
+        id: 1,
+        pic: pic1
+      },
+      {
+        id: 2,
+        pic: pic2
+      },
+      {
+        id: 3,
+        pic: pic3
+      }
+    ]
+  }
+  handleClickPick = () => {
+    console.log("you got clicked")
+  }
+
+  render(){
+    //secret spot to console log
+    var self = this
+    console.log("see what's printing", this.state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+<div>
+{
+  this.state.picArray.map(function(singleDude){
+    //will give html here
+    return(
+      <img onClick = {self.handleClickPick} src={singleDude.pic} />
+      
+    )
+  })
+}
+
+</div>
+
     </div>
   );
+}
 }
 
 export default App;
